e1afb217f07b9c0661eb3a615ed6039ba61a77dc
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-09-30 15:45:08 -0400

    solve all maze

--- maze-all.js
+++ maze-all.js
@@ -2,31 +2,33 @@ function mazeAll(
   array,
   y = 0,
   x = 0,
-  index = 0,
   direction = 'S',
-  solution = []
+  solution = [],
+  results = []
 ) {
   if (x < 0 || y < 0) return;
   if (x >= array[0].length || y >= array.length) return;
 
-  solution[index] = direction;
-  index++;
+  solution = solution.concat(direction);
 
   if (array[y][x] === 'e') {
-    console.log(`Found path to exit: ${solution}`);
-    return;
+    results.push(solution);
   }
 
   if (array[y][x] === ' ') {
     array[y][x] = 'x';
-    mazeAll(array, y, x - 1, index, 'L', solution);
-    mazeAll(array, y - 1, x, index, 'U', solution);
-    mazeAll(array, y, x + 1, index, 'R', solution);
-    mazeAll(array, y + 1, x, index, 'D', solution);
+    mazeAll(array, y, x - 1, 'L', solution, results);
+    // solution passed in ['S'], it tries x - 1, so it skips to next:
+    mazeAll(array, y - 1, x, 'U', solution, results);
+    // same as above
+    mazeAll(array, y, x + 1, 'R', solution, results);
+    // solution passed in ['S'], it tries x + 1, so it changes solution to ['S', 'R'] and continues
+    mazeAll(array, y + 1, x, 'D', solution, results);
+    // solution passed in as ['SRR']
     array[y][x] = ' ';
   }
 
-  index--;
+  return results;
 }
 
 const smallMaze = [[' ', ' ', ' '], [' ', '*', ' '], [' ', ' ', 'e']];
@@ -38,5 +40,13 @@ const bigMaze = [
   [' ', ' ', ' ', ' ', ' ', ' ', 'e']
 ];
 
-console.log(mazeAll(smallMaze));
-console.log(mazeAll(bigMaze));
+console.log(`
+Small maze results:
+  ${mazeAll(smallMaze).join('\n  ')}
+
+Big maze results:
+  ${mazeAll(bigMaze).join('\n  ')}
+`);
+
+// alternate: use strings instead of arrays for solution
+// be careful of arrays being passed as reference (would be same with objects)
