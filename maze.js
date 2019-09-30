7af16b64870c943e685287c262c565b440980e2d
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-09-30 12:31:49 -0400

    solve first maze

--- maze.js
+++ maze.js
@@ -1,17 +1,23 @@
-function maze (array, y=0 , x=0) {
+function maze(array, y = 0, x = 0) {
+  if (array[y][x + 1] === 'e') return ['R'];
+  if (array[y + 1][x] === 'e') return ['D'];
+  if (x > 0 && array[y][x - 1] === 'e') return ['L'];
+  if (y > 0 && array[y - 1][x] === 'e') return ['U'];
 
-
- if (array[y][x +1] === ' '){
-     maze(array, y, x+1) 
-     push.ans(r)
- } else if(array[y +1][x] === ' ') {
-    maze(array, y +1, x) 
- }
+  if (array[y][x + 1] === ' ') return ['R', ...maze(array, y, x + 1)];
+  if (array[y + 1][x] === ' ') return ['D', ...maze(array, y + 1, x)];
+  if (array[y][x - 1] === ' ') return ['L', ...maze(array, y, x - 1)];
+  if (array[y + 1][x] === ' ') return ['U', ...maze(array, y - 1, x)];
 }
 
-
-let mySmallMaze = [
-    [' ', ' ', ' '],
-    [' ', '*', ' '],
-    [' ', ' ', 'e']
+const smallMaze = [[' ', ' ', ' '], [' ', '*', ' '], [' ', ' ', 'e']];
+const bigMaze = [
+  [' ', ' ', ' ', '*', ' ', ' ', ' '],
+  ['*', '*', ' ', '*', ' ', '*', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [' ', '*', '*', '*', '*', '*', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', 'e']
 ];
+
+console.log(maze(smallMaze));
+console.log(maze(bigMaze));
