e98074b63959fff610b99f74ef2528c4584b85c4
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-09-30 16:37:55 -0400

    styling

--- anagrams.js
+++ anagrams.js
@@ -1,22 +1,19 @@
-function anagrams (str) {
-    const results = []
-    if (str.length === 1){
-        results.push(str)
-    } else {
-        for (let i=0; i < str.length; i++){
-            const first = str[i] 
+function anagrams(str) {
+  const results = [];
+  if (str.length === 1) {
+    results.push(str);
+  } else {
+    for (let i = 0; i < str.length; i++) {
+      const first = str[i];
 
-            const remain = str.slice(0, i) + str.slice(i + 1)
-            const permutations = anagrams(remain)
-            permutations.forEach(p => {
-                results.push(first + p)
-            });
-        }
+      const remain = str.slice(0, i) + str.slice(i + 1);
+      const permutations = anagrams(remain);
+      permutations.forEach(p => {
+        results.push(first + p);
+      });
     }
-    return results;
+  }
+  return results;
 }
 
-
-console.log(anagrams('ab'))
-
-
+console.log(anagrams('ab'));
