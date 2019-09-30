462337bd75b11f43ca059d9b7c2c87127f46c144
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-09-30 13:34:43 -0400

    begin orgChart

---
+++ orgChart.js
@@ -0,0 +1,23 @@
+function chart(obj) {}
+
+const object = {
+  Zuckerberg: {
+    Schroepfer: {
+      Bosworth: ['Steve', 'Kyle', 'Andra'],
+      Zhao: ['Richie', 'Sofia', 'Jen']
+    },
+    Schrage: {
+      VanDyck: ['Sabrina', 'Michelle', 'Josh'],
+
+      Swain: ['Blanch', 'Tom', 'Joe']
+    },
+
+    Sandberg: {
+      Goler: ['Eddie', 'Julie', 'Annie']
+    },
+    Hernandez: ['Rowi', 'Inga', 'Morgan'],
+    Moissinac: ['Amy', 'Chuck', 'Vinni'],
+    Kelley: ['Eric', 'Ana', 'Wes']
+  }
+};
+console.log(chart(object));
