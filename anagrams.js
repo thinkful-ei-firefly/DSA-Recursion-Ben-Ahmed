8920b35c58d334178b4b3f7972c2896fd52c05a9
Ahmed <ahmed.max7586@gmail.com>
2019-09-30 16:37:30 -0400

    added anagrams

---
+++ anagrams.js
@@ -0,0 +1,22 @@
+function anagrams (str) {
+    const results = []
+    if (str.length === 1){
+        results.push(str)
+    } else {
+        for (let i=0; i < str.length; i++){
+            const first = str[i] 
+
+            const remain = str.slice(0, i) + str.slice(i + 1)
+            const permutations = anagrams(remain)
+            permutations.forEach(p => {
+                results.push(first + p)
+            });
+        }
+    }
+    return results;
+}
+
+
+console.log(anagrams('ab'))
+
+
