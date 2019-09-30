c1a5a114626faa8088a102568c489213e4bfb47a
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-09-30 17:11:50 -0400

    solve all

--- orgChart.js
+++ orgChart.js
@@ -1,23 +1,82 @@
-function chart(obj) {}
+function indenter(chart, indent = 0) {
+  for (const key in chart) {
+    console.log(' '.repeat(indent), key);
+    indenter(chart[key], indent + 4);
+  }
+}
 
-const object = {
+// function indenter(chart, indent = 0, result = '') {
+//   for (const key in chart) {
+//     result += `${' '.repeat(indent)}${key}`;
+//     indenter(chart[key], indent + 4, result);
+//   }
+//   return result;
+// }
+
+let object = {
   Zuckerberg: {
     Schroepfer: {
-      Bosworth: ['Steve', 'Kyle', 'Andra'],
-      Zhao: ['Richie', 'Sofia', 'Jen']
+      Bosworth: {
+        Steve: {},
+        Kyle: {},
+        Andra: {}
+      },
+      Zhao: {
+        Richie: {},
+        Sofia: {},
+        Jen: {}
+      },
+      Badros: {
+        John: {},
+        Mike: {},
+        Pat: {}
+      },
+      Parikh: {
+        Zach: {},
+        Ryan: {},
+        Tes: {}
+      }
     },
     Schrage: {
-      VanDyck: ['Sabrina', 'Michelle', 'Josh'],
-
-      Swain: ['Blanch', 'Tom', 'Joe']
+      VanDyck: {
+        Sabrina: {},
+        Michelle: {},
+        Josh: {}
+      },
+      Swain: {
+        Blanch: {},
+        Tom: {},
+        Joe: {}
+      },
+      Frankovsky: {
+        Jasee: {},
+        Brian: {},
+        Margaret: {}
+      }
     },
-
     Sandberg: {
-      Goler: ['Eddie', 'Julie', 'Annie']
-    },
-    Hernandez: ['Rowi', 'Inga', 'Morgan'],
-    Moissinac: ['Amy', 'Chuck', 'Vinni'],
-    Kelley: ['Eric', 'Ana', 'Wes']
+      Goler: {
+        Eddie: {},
+        Julie: {},
+        Annie: {}
+      },
+      Hernandez: {
+        Rowi: {},
+        Inga: {},
+        Morgan: {}
+      },
+      Moissinac: {
+        Amy: {},
+        Chuck: {},
+        Vinni: {}
+      },
+      Kelley: {
+        Eric: {},
+        Ana: {},
+        Wes: {}
+      }
+    }
   }
 };
-console.log(chart(object));
+
+console.log(indenter(object));
